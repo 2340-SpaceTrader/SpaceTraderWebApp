5396ed167634964914a327ca21266210d6e13a81
K <kiettran8795@gmail.com>
2019-04-23 05:35:22 -0400

    add firebase and vuex store

--- src/main.js
+++ src/main.js
@@ -4,11 +4,31 @@ import App from './App.vue'
 
 import router from './router'
 
+// import {store} from './store.js'
 
 Vue.config.productionTip = false
 
+
+// const fb = require('./firebaseConfig.js')
+
+// let app
+// fb.auth.onAuthStateChanged(user => {
+//     if (!app) {
+//         app = new Vue({
+//             el: '#app',
+//             router,
+//             // store,
+//             render: h => h(App)
+//         })
+//     }
+// })
+
 new Vue({
-  render: h => h(App),
+  el: '#app',
   router,
+  // store,
+  render: h => h(App),
+  
 
 }).$mount('#app')
+
