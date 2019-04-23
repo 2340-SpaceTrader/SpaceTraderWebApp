70e2a05a2a64c9bc76782577f45f09023c83d13d
Tu Nguyen <tnguyen496@gatech.edu>
2019-04-23 06:43:09 -0400

    create market place screen

---
+++ src/components/MarketPlace.vue
@@ -0,0 +1,69 @@
+<template>
+    
+    <v-layout  align-center justify-center>
+      <v-flex xs12 sm8 md5>
+        <v-card class="elevation-12">
+          <v-toolbar dark color="primary">
+            <v-toolbar-title>Space Trader</v-toolbar-title>
+          </v-toolbar>
+
+          <!-- <v-card-actions> -->
+            <v-layout column align-center justify-space-around>
+            <p class="text-md-Left">Name: {{name}}</p>
+            <p class="text-md-Left">Fuel: {{fuel}}</p>
+            <p class="text-md-Left">Planet: {{planet}}</p>
+            <p class="text-md-Left">Position: {{position}}</p>
+            <p class="text-md-Left">Resource: {{resource}}</p>
+            </v-layout>
+
+            <v-layout row align-start justify-space-around>
+                <v-flex xs5 offset-xs1>
+                    <v-overflow-btn
+                        :items="contain_site"
+                        label="Please Select One"
+                        target="#dropdown-example"
+                    ></v-overflow-btn>
+                </v-flex>
+                <v-flex xs5 offset-xs1>
+                    <v-overflow-btn
+                        :items="contain_site"
+                        label="Please Select One"
+                        target="#dropdown-example"
+                    ></v-overflow-btn>
+                </v-flex>
+            </v-layout>
+
+
+            <v-layout row align-start justify-space-around>
+                <v-btn large color="primary" to="/RegNav">BUY ITEMS</v-btn>
+                <v-btn large color="primary" to="/RegNav">SELL ITEMS</v-btn>
+            </v-layout>
+
+            
+
+            <v-layout row align-start justify-space-around>
+                <v-btn color="primary" to="/RegNav">BACK</v-btn>
+                <v-btn color="primary" to="/RegNav">DONE</v-btn>
+            </v-layout>
+
+
+          <!-- </v-card-actions> -->
+        </v-card>
+      </v-flex>
+    </v-layout>
+</template>
+
+<script>
+  export default {
+    data () {
+        
+      return {
+        contain_site: ['Beginner', 'Easy', 'Normal', 'Hard', 'Impossible'],
+        name: 'Hello', fuel: "200.0", planet: "Planet123", position: "(2,5)", resource: "Resource123"
+      }
+    },
+    props: {
+      source: String
+    }
+  }
+</script>
