b8cb68894baac37742f090bbea525abd6134eadd
Duc Le <ducle147@gmail.com>
2019-04-23 06:01:54 -0400

    Finish RegUser, TravelOption

---
+++ src/components/RegUser.vue
@@ -0,0 +1,89 @@
+<template>
+  <v-layout align-center justify-center>
+    <v-flex xs12 sm8 md4>
+      <v-card class="elevation-12">
+        <v-toolbar dark color="primary">
+          <v-toolbar-title>Register User</v-toolbar-title>
+        </v-toolbar>
+        <v-card-text>
+          <v-form>
+            <v-text-field
+              id="Firstname"
+              name="Firstname"
+              label="First Name"
+              type="Text"
+              v-model="passwordStr"
+              :error-messages="passwordErrors"
+              required
+              @input="$v.passwordStr.$touch()"
+              @blur="$v.passwordStr.$touch()"
+            ></v-text-field>
+            <v-text-field
+              id="Lastname"
+              name="Lastname"
+              label="Last Name"
+              type="Lastname"
+              v-model="passwordStr"
+              :error-messages="passwordErrors"
+              required
+              @input="$v.passwordStr.$touch()"
+              @blur="$v.passwordStr.$touch()"
+            ></v-text-field>
+            <v-text-field
+              id="Username"
+              name="Username"
+              label="Username"
+              type="Username"
+              v-model="passwordStr"
+              :error-messages="passwordErrors"
+              required
+              @input="$v.passwordStr.$touch()"
+              @blur="$v.passwordStr.$touch()"
+            ></v-text-field>
+            <v-text-field
+              id="password"
+              prepend-icon="lock"
+              name="password"
+              label="Password"
+              type="password"
+              v-model="passwordStr"
+              :error-messages="passwordErrors"
+              required
+              @input="$v.passwordStr.$touch()"
+              @blur="$v.passwordStr.$touch()"
+            ></v-text-field>
+            <v-text-field
+              id="ConfirmPassword"
+              prepend-icon="lock"
+              name="ConfirmPassword"
+              label="Confirm Password"
+              type="ConfirmPassword"
+              v-model="passwordStr"
+              :error-messages="passwordErrors"
+              required
+              @input="$v.passwordStr.$touch()"
+              @blur="$v.passwordStr.$touch()"
+            ></v-text-field>
+            <v-text-field
+              prepend-icon="person"
+              name="email"
+              label="Email"
+              type="text"
+              v-model="emailStr"
+              :error-messages="emailErrors"
+              required
+              @input="$v.emailStr.$touch()"
+              @blur="$v.emailStr.$touch()"
+            ></v-text-field>
+            <v-layout align-center justify-center>
+            </v-layout>
+            <v-layout align-center justify-center>
+              <v-btn color="primary" to="/RegNav">Back</v-btn>
+              <v-btn color="primary" to="/RegNav">Register</v-btn>
+            </v-layout>
+          </v-form>
+        </v-card-text>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
---
+++ src/components/TravelOption.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-layout align-center justify-center>
+    <v-flex xs12 sm12 md8>
+      <v-card class="elevation-12">
+        <v-toolbar dark color="primary">
+          <v-toolbar-title>Travel Option</v-toolbar-title>
+        </v-toolbar>
+
+        <v-card-text>
+          <v-form>
+
+<template>
+  <v-container fluid>
+    <v-layout row wrap align-center>
+      <v-flex xs6>
+       
+        <v-subheader>Destination</v-subheader>
+      </v-flex>
+      <v-flex xs6>
+        <v-select
+          v-model="e2"
+          :items="states"
+          menu-props="auto"
+          hide-details
+          label="Select"
+          single-line
+        ></v-select>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+            <v-layout align-center justify-center>
+            </v-layout>
+            <v-layout align-center justify-center>
+              <v-btn color="primary" to="/RegNav">Back</v-btn>
+              <v-btn color="primary" @click="onLogin">Next</v-btn>
+            </v-layout>
+          </v-form>
+        </v-card-text>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  data () {
+    return  {
+    emailStr: "",
+    passwordStr: "" ,   
+        e1: null,
+        e2: null,
+        e3: null,
+        e4: null,
+        items: [
+          { text: 'State 1' },
+          { text: 'State 2' },
+          { text: 'State 3' },
+          { text: 'State 4' }
+        ],
+        states: [
+        'Place 1','Place 2','Place 3'
+        ]
+      }
+  },
+};
+</script>
